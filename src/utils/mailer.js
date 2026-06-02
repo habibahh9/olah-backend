754a9252a7c0649d36946e7a2d5056b14b2d40b9
Putrianisa2526 <putrianisacc4@gmail.com>
2026-06-02 14:07:09 +0700

    fix: Update mailer configuration for better compatibility

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -2,13 +2,16 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-   port: 587,
+  port: 587,
   family: 4,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false  // ← tambahkan ini
+  }
 });
 
 const sendOtpEmail = async (toEmail, otp) => {
