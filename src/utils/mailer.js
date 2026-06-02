feac464745303c138d0af07a0291c74f6b637e0c
Putrianisa2526 <putrianisacc4@gmail.com>
2026-06-02 14:01:28 +0700

    fix: Update mailer configuration for better compatibility

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -2,9 +2,9 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
+   port: 587,
   family: 4,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
