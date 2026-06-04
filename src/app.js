3c909a80c94883598616661e2f9f071a860199b7
Habibah <maritahabibah6726@gmail.com>
2026-06-04 10:55:54 +0700

    Update CORS origin for API endpoint

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['https://olah-frontend.vercel.app', 'http://localhost:5173'],
+  origin: ['https:// olah-api.vercel.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
